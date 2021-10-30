088e15a0da13791494a7986da2e158f3bf197076
Vince Van Dintel <vince@vandintel.com>
2021-10-30 11:48:20 -0400

    Added default error middleware

--- src/index.js
+++ src/index.js
@@ -20,9 +20,15 @@ app.use(expressWinston.logger({
         winston.format.colorize(),
         winston.format.json()
     )
-}))
+})) 
+
 app.get('/', routes.helloRoute)
 app.get('/daily-forecast', routes.dailyForecast)
+
+app.use(function(err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send({ message: 'Sorry, a server occured. Please try again later' })
+})
 
 io.on('connection', (socket) => {
     console.log('a user connected')
--- src/routes/weather/index.js
+++ src/routes/weather/index.js
@@ -1,16 +1,18 @@
 const axios = require('axios')
 
-const dailyForecast = async (request, reply) => {
+const dailyForecast = async (request, reply, next) => {
     
-    const baseUrl = 'https://api.openweathermap.org/data/2.5/forecast/daily'
+    // const baseUrl = 'https://api.openweathermap.org/data/2.5/forecast/daily'
+    const baseUrl = 'https://api.openweathermap.org/data/2.5/forecast/weather'
     const requestUrl = `${baseUrl}?zip=${request.query.zipcode}&appid=${process.env.OWM_API_KEY}&cnt=${request.body.daysCount}`
 
     try {
         const dailyForecast = await axios.get(requestUrl)
         reply.send({ dailyForecast: dailyForecast.body })
     } catch (error) {
-        console.log({error: error.me})
-        reply.send({ error: 'Error fetching weather data' })
+        console.log({error: error.stack})
+        const dailyForecastFetchError = new Error('Error fetching weather data' )
+        next(dailyForecastFetchError)
     }
 }
 
